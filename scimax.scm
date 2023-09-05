bcb6660e2cf68ac9677dfde08b9671bd63c12f20
David Conner <aionfork@gmail.com>
2023-09-05 13:39:30 -0400

    setup a guix profile for scimax

---
+++ scimax.scm
@@ -0,0 +1,12 @@
+(specifications->manifest
+  '("nss-certs"
+    "emacs-next-pgtk"
+    "git"
+    "git:send-email"
+
+    "aspell"
+    "aspell-dict-en"
+
+    "font-juliamono"
+    "font-google-noto"
+    "font-google-noto-emoji"))
